6bc0261c7010ef3d82683dd319e8148509f61d28
AlDIY <87589371+dosipod@users.noreply.github.com>
2023-12-04 00:03:17 +0300

    Update XYmapper.js

--- XYmapper.js
+++ XYmapper.js
@@ -304,8 +304,8 @@ function printMap() {
   mapHTML = "";
   ledindex = 0;
   mapHTML += '<PRE>';
-
-  mapHTML += '{"n":"matrix","map":[';
+ // mapHTML += '{"n":"matrix","map":[';
+  mapHTML += '{"n":"matrix","map",'+'"width":' + xdim + ';
   for (y = 0; y < ydim; y++) {
     mapHTML += '<BR>';
     for (x = 0; x < xdim; x++) {
